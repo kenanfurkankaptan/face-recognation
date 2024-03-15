dbed18651485486dace07d70c736c66cc4fba666
kfk <furkan.kaptan@infinidium.com.tr>
2024-03-15 14:12:09 +0300

    gitignore update

--- src/constants.cpp
+++ src/constants.cpp
@@ -3,8 +3,8 @@
 namespace Constants {
 	namespace Path {
 		const std::string face_detection = "models/face detection/haarcascade_frontalface_default.xml";
-		const std::string face_recognation = "models/face recognize";
-		const std::string face_recognation_info = "models/face recognize/model_info.json";
+		const std::string face_recognation = "models/face recognation";
+		const std::string face_recognation_info = "models/face recognation/model_info.json";
 		const std::string data = "data";
 	}  // namespace Path
 
